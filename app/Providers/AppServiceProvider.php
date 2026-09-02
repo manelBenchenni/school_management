5e89dd97aac4150f63cfbd7eec962f8a71224bdd
manelBenchenni <manelmbnchenni@gmail.com>
2026-09-02 22:32:23 +0100

    fixing url problem

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,6 +5,7 @@ namespace App\Providers;
 use Illuminate\Support\Facades\Vite;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -23,8 +24,9 @@ class AppServiceProvider extends ServiceProvider
     {
         Vite::prefetch(concurrency: 3);
         Schema::defaultStringLength(191);
-         if ($this->app->environment('production')) {
-        URL::forceScheme('https');
-    }
+
+        if ($this->app->environment('production')) {
+            URL::forceScheme('https');
+        }
     }
 }
